d2880089bd0dfe946878e76c05d84682bd80f9dd
Darien-Gómez-dev <135762123+Theblasterbarrow@users.noreply.github.com>
2026-07-28 01:37:14 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+/* Service Worker - Levantamientos OPS/KOICA - v2 */
+const CACHE='lev-v2';
+const ASSETS=[
+  './',
+  './index.html',
+  './levantamiento-sip.html',
+  './levantamiento-insumos.html',
+  './levantamiento-personal-aipeo.html',
+  './manifest-sip.webmanifest',
+  './manifest-insumos.webmanifest',
+  './manifest-aipeo.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+/* Estrategia: caché primero, actualización en segundo plano */
+self.addEventListener('fetch', e=>{
+  if(e.request.method!=='GET') return;
+  const url=new URL(e.request.url);
+  if(url.origin!==location.origin) return;
+  e.respondWith(
+    caches.match(e.request).then(cached=>{
+      const fresh=fetch(e.request).then(resp=>{
+        if(resp && resp.ok){
+          const copy=resp.clone();
+          caches.open(CACHE).then(c=>c.put(e.request, copy));
+        }
+        return resp;
+      }).catch(()=>cached);
+      return cached || fresh;
+    })
+  );
+});
